5d45b411b35ffda5de3c83d6a5c0007ba861e869
Jean Carlos Ramos <jeancarlos.rcj@gmail.com>
2023-10-15 15:00:02 -0300

    Projeto inicial

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const QuizApp());
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> marcadordePontos = [];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'As perguntas serão exibidas aqui.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.black),
+              child: const Text(
+                'Verdadeiro',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  marcadordePontos.add(const Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.black),
+              child: const Text(
+                'Falso',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  marcadordePontos.add(const Icon(
+                    Icons.close,
+                    color: Colors.grey,
+                  ));
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: marcadordePontos,
+        )
+      ],
+    );
+  }
+}
+
+/*
+pergunta1: 'O metrô é um dos meios de transporte mais seguros do mundo', verdadeiro,
+pergunta2: 'A culinária brasileira é uma das melhores do mundo.', verdadeiro,
+pergunta3: 'Vacas podem voar, assim como peixes utilizam os pés para andar.', falso,
+*/
